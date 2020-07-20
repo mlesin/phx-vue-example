331b596dfe2cfbf185f103325421e9ae67f71aa4
Mikhail Lesin <mlesin@mrpost.ru>
2020-07-21 00:01:42 +0300

    more functional way to validate

--- frontend/src/socketApi.ts
+++ frontend/src/socketApi.ts
@@ -1,72 +1,61 @@
 import { Assignments } from "./plugins/vue-phx";
 import store from "./store";
-import * as t from "io-ts";
+import * as io from "io-ts";
+import { PathReporter } from "io-ts/lib/PathReporter";
 import { pipe } from "fp-ts/lib/function";
 import * as E from "fp-ts/lib/Either";
-import Either = E.Either;
+import * as C from "fp-ts/lib/Console";
 
 /** Helper function for describing optional types */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function optional<RT extends t.Any>(
+function optional<RT extends io.Any>(
   type: RT,
   name = `${type.name} | undefined`
-): t.UnionType<[RT, t.UndefinedType], t.TypeOf<RT> | undefined, t.OutputOf<RT> | undefined, t.InputOf<RT> | undefined> {
-  return t.union<[RT, t.UndefinedType]>([type, t.undefined], name);
+): io.UnionType<[RT, io.UndefinedType], io.TypeOf<RT> | undefined, io.OutputOf<RT> | undefined, io.InputOf<RT> | undefined> {
+  return io.union<[RT, io.UndefinedType]>([type, io.undefined], name);
 }
 
 // Describing validators for payloads we are expecting from our backend (`V` is short for `Validator`)
 /** Message structure */
-const MessageV = t.type(
+const MessageV = io.type(
   {
     /** Sender's ID */
-    sender: t.string,
+    sender: io.string,
     /** Message text */
-    message: t.string
+    message: io.string
   },
   "Message"
 );
-const JoinReplyV = t.type(
+const JoinReplyV = io.type(
   {
-    status: t.union([t.literal("ok"), t.literal("error")]),
-    response: t.literal("join")
+    status: io.union([io.literal("ok"), io.literal("error")]),
+    response: io.literal("join")
   },
   "Pure Reply"
 );
 
 /** Phoenix Reply structure */
-const MessageReplyV = t.type(
+const MessageReplyV = io.type(
   {
-    status: t.union([t.literal("ok"), t.literal("error")]),
+    status: io.union([io.literal("ok"), io.literal("error")]),
     response: MessageV
   },
   "Message Reply"
 );
 
-const ReplyV = t.union([JoinReplyV, MessageReplyV]);
+const ReplyV = io.union([JoinReplyV, MessageReplyV]);
 
 // Static types for io-ts descriptions above
-export type Message = t.TypeOf<typeof MessageV>;
-export type JoinReply = t.TypeOf<typeof JoinReplyV>;
-export type MessageReply = t.TypeOf<typeof MessageReplyV>;
-export type Reply = t.TypeOf<typeof ReplyV>;
+export type Message = io.TypeOf<typeof MessageV>;
+export type JoinReply = io.TypeOf<typeof JoinReplyV>;
+export type MessageReply = io.TypeOf<typeof MessageReplyV>;
+export type Reply = io.TypeOf<typeof ReplyV>;
 
 /** Helper function for validate types for messages we are receiving from our backend */
-function validate<T>(codec: t.Type<T>, handler: (typedPayload: T) => void, errorHandler?: (errors: t.Errors) => void) {
+function validate<T>(codec: io.Type<T>, handler: (typedPayload: T) => void) {
   return (payload: unknown) => {
-    const result: Either<t.Errors, T> = codec.decode(payload);
-    pipe(
-      result,
-      E.fold(
-        err => {
-          if (errorHandler) {
-            errorHandler(err);
-          } else {
-            console.error("Return type does not match expectations:", err);
-          }
-        },
-        res => handler(res)
-      )
-    );
+    const result = codec.decode(payload);
+    pipe(result, E.mapLeft(C.error(PathReporter.report(result).join("\n"))), E.map(handler));
   };
 }
 
